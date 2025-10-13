c838cce5f3ce426b1f930fc8d10196b27721899a
Nicolas Grave <nyko-23@hotmail.com>
2025-10-13 13:32:01 -0300

    Include an inspiring sentence at the end of the page.

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,15 @@
 
       <div id="modals"></div>
     </main>
+
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import Header from '#components/App/Header.vue';
+import Footer from '#components/App/Footer.vue';
 import useLanguage from '#composables/language.js';
 import Home from '#views/Home.vue';
 
---
+++ src/components/App/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-footer">
+    {{ t("footer.message") }}
+  </div>
+</template>
+
+<script setup>
+import useI18n from '#composables/i18n.js';
+
+const { t } = useI18n();
+</script>
+
+<style lang="scss" scoped>
+.app-footer {
+  font-family: 'Just Me Again Down Here', cursive;
+  font-weight: 400;
+  font-size: var(--fs-title);
+  background: var(--bg-secondary);
+  padding: 80px 0;
+  text-align: center;
+}
+</style>
